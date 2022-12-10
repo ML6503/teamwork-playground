9ac8e7636e0f69e12a4342242057bc217a6b2dd6
ML6503 <ml6503@gmail.com>
2022-12-10 19:36:56 +0200

    feat: add websocket chat

--- client/src/reactapp.tsx
+++ client/src/reactapp.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import {IMessage } from '../../interface/IMessage';
 interface IAppProps {
   onClick: () => void;
   text: string;
@@ -47,18 +47,93 @@ export function CheckBox(props: ICheckBoxProps) {
 
 export function RequestServer() {
   const [response, setResponse] = useState("");
+  const [socket, setSocket ] = useState<WebSocket>(null);
+  const [inputMsg, setInputMsg] = useState('');
+  const [messages, setMessages] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    const websocket = new WebSocket("ws://localhost:3000");
+    websocket.onmessage = (msg) => {
+      
+      const parsedMsg: IMessage = JSON.parse(msg.data);
+      console.log('parsedMsg type', parsedMsg.type);
+
+      switch (parsedMsg.type) {
+        case 'chatMsg':
+          {
+            console.log('parsedMsg data', parsedMsg.data);
+            // messages.push(msg.data);
+            setMessages((prev) => {
+              return [parsedMsg.data, ...prev]
+            });
+            
+           
+           
+            // clients.forEach( c => {
+            //   c.sendUTF(JSON.stringify(chatMsg));
+            // });
+            break;
+          }
+  
+          case 'chatHistory' : {
+            const msgList: Array<string> = JSON.parse(parsedMsg.data);
+            console.log('msg list: ', msgList);
+            setMessages(msgList.reverse());
+            break;
+          }
+          
+        default:
+          break;
+      }
+      
+     
+    }; 
+    websocket.onopen = () => {
+      console.log('connected');
+      setSocket(websocket);
+
+      const request: IMessage = {
+        type: 'chatHistory',
+        data: '',
+        id: 3
+      };
+      websocket.send(JSON.stringify(request));
+    };
+
+    return () => {
+      websocket.close();
+    };
+
+  }, []);
+
   return (
     <div>
       <span>{response}</span>
+      <input onChange={(e) => {
+       setInputMsg( e.target.value);
+      }} type="text" placeholder="Write your message here" value={inputMsg}/>
       <button
         onClick={() => {
-          fetch("http://localhost:3000")
-            .then((data) => data.text())
-            .then((data) => setResponse(data));
+
+          const request: IMessage = {
+            type: 'chatMsg',
+            data: inputMsg,
+            id: 3
+          };
+          socket.send(JSON.stringify(request));
+          // socket.send(inputMsg);
+          setInputMsg('');
+          // fetch("http://localhost:3000")
+          //   .then((data) => data.text())
+          //   .then((data) => setResponse(data));
+
         }}
       >
         New button
       </button>
+      <div>
+        {messages.map((m , i) => (<span key={m + i}>{m}</span>))}
+      </div>
     </div>
   );
 }
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,17 @@
 import http from "http";
 import { DataBase } from "./src/database/db";
+import * as WebSocket from 'websocket';
+import {IMessage } from '../interface/IMessage';
 const db = new DataBase();
 
 const server = http.createServer((req, res) => {
+
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Access-Control-Allow-Origin', '*' );
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type',);
+
   console.log((new Date()) + ' Received request for ' + req.url);
   // /register?name=JohnDoe&age=42
   const reqData = req.url?.split("?");
@@ -28,4 +37,65 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
   console.log((new Date()) + ' Server is listening port 3000');
+});
+
+const websocket = new  WebSocket.server({ httpServer: server });
+
+const messages: Array<string> = [];
+
+const clients: Array<WebSocket.connection>= [];
+
+websocket.on('request', (e) => {
+  const client = e.accept();
+  clients.push(client);
+
+  console.log('connect');
+
+  client.on('message', msg => {
+    console.log(msg);
+    if (msg.type !==  'utf8') {
+      return;
+    }
+
+    const parsedMsg: IMessage = JSON.parse(msg.utf8Data);
+    console.log('parsedMsg', parsedMsg);
+
+    switch (parsedMsg.type) {
+      case 'chatMsg':
+        {
+          messages.push(parsedMsg.data);
+          
+          const chatMsg: IMessage = {
+            type: 'chatMsg',
+            data: parsedMsg.data,
+            id: 1
+          };
+
+          clients.forEach( c => {
+            c.sendUTF(JSON.stringify(chatMsg));
+          });
+          break;
+        }
+
+        case 'chatHistory' : {
+          const historyChat: IMessage = {
+            type: 'chatHistory',
+            data: JSON.stringify(messages),
+            id: 0
+          };
+          client.sendUTF(JSON.stringify(historyChat));
+          break;
+        }
+        
+      default:
+        break;
+    }
+    
+  
+  });
+
+  client.on('close', () => {
+    clients.splice(clients.indexOf(client), 1)
+  });
+
 });
